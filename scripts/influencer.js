de1daf454b90958f29dd1bdcdcb7591a6bdf5299
LuizFried <luifried50@gmail.com>
2025-02-11 04:18:50 -0300

    staff update

---
+++ scripts/influencer.js
@@ -0,0 +1,17 @@
+document.getElementById("influencerID").addEventListener("click", function() {
+    document.getElementById("conteudoID").innerHTML =`
+    <a href="#" onclick="location.reload(); return false;"><i class="fa-solid fa-arrow-left"></i></a>
+    <h1 class="titulo__influencer">Influencers</h1>
+    <div class="box__influencer">
+        <div class="influencer">
+            <img class="img__influencer" src="./src/assets/influencer/turmadobatz.png" alt="Foto do influencer">
+            <h1 class="nome__influencer">BatzOvo</h1>
+            <span class="span__ancora__link">
+                <a class="ancora__link" href="https://www.twitch.tv/turmadobatz" target="blank"><i class="fa-brands fa-twitch"></i></a>
+                <a class="ancora__link" href="https://www.youtube.com/@PanelaAlbina" target="blank"><i class="fa-brands fa-youtube"></i></a>
+            </span>
+        </div>
+
+        
+    </div>`;
+});
