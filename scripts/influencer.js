ce63375fadfafbf3fcf497456315fb89afad8c3f
LuizFried <luifried50@gmail.com>
2025-02-12 05:14:00 -0300

    correção fonte e borda

--- scripts/influencer.js
+++ scripts/influencer.js
@@ -12,6 +12,15 @@ document.getElementById("influencerID").addEventListener("click", function() {
             </span>
         </div>
 
+        <div class="influencer">
+            <img class="img__influencer" src="./src/assets/influencer/marquesbr.jpg" alt="Foto do influencer">
+            <h1 class="nome__influencer">M4rquesBR</h1>
+            <span class="span__ancora__link">
+                <a href="https://www.twitch.tv/m4rquesbr7" target="blank"><i id="twitchID" class="fa-brands fa-twitch"></i></a>
+                <a href="https://www.youtube.com/@M4QUESBR" target="blank"><i id="youtubeID" class="fa-brands fa-youtube"></i></a>
+            </span>
+        </div>
+
         
     </div>`;
 });
