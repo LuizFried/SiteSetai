27d756e5e481d8f5cf767d330dfea7a93d8969e4
LuizFried <luifried50@gmail.com>
2025-02-13 04:28:51 -0300

    att links uteis

--- scripts/uteis.js
+++ scripts/uteis.js
@@ -10,6 +10,13 @@ document.getElementById("utilidadesID").addEventListener("click", function() {
             <a href="https://triky313.github.io/AlbionOnline-StatisticsAnalysis/" target="_blank" id="analitics">
                 <h3>Statistics Analysis</h3>
                 <i class="fa-solid fa-chart-pie"></i>
+                <ul class="ul_links">
+                    <li>-Estatísticas de Combate</li>
+                    <li>-Análise de Guildas</li>
+                    <li>-Ranking de Jogadores</li>
+                    <li>-Gráficos Interativos</li>
+                    <li>-Relatórios Detalhados</li>
+                </ul>
             </a>
         </span>
 
@@ -17,6 +24,27 @@ document.getElementById("utilidadesID").addEventListener("click", function() {
             <a href="https://albiononlinegrind.com" target="_blank" id="grind">
                 <h3>Albion Online Grind</h3>
                 <i class="fa-solid fa-sitemap"></i>
+                <ul class="ul_links">
+                    <li>-Guias de Grind</li>
+                    <li>-Dicas de Recursos</li>
+                    <li>-Comparação de Equipamentos</li>
+                    <li>-Rotas de Coleta</li>
+                    <li>-Otimizando Habilidades</li>
+                </ul>
+            </a>
+        </span>
+
+        <span class="span_murder">
+            <a href="https://murderledger.albiononline2d.com" target="_blank" id="murder">
+                <h3>Murder Ledger</h3>
+                <i class="fa-solid fa-book-skull"></i>
+                <ul class="ul_links">
+                    <li>-Rastrear mortes</li>
+                    <li>-Analisar builds</li>
+                    <li>-Ver rankings</li>
+                    <li>-Consultar estatísticas</li>
+                    <li>-Explorar clãs</li>
+                </ul>
             </a>
         </span>
 
