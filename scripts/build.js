1ae85b6cdb148a39fb4d954f968c6f59f914b5a4
LuizFried <luifried50@gmail.com>
2025-02-13 22:17:33 -0300

    ancora temporaria

---
+++ scripts/build.js
@@ -0,0 +1,12 @@
+document.getElementById("buildsID").addEventListener("click", function() {
+    document.getElementById("conteudoID").innerHTML =`
+    <div class="conteudo__builds">
+        <h1>BUILDS AVALON PRESS</h1>
+        <span class="builds">
+            <a class="button__press__1" href="./src/assets/builds/SetaiPressAvalon.png" target="_blank">Avalon Press 1</a>
+            <a class="button__press__2" href="./src/assets/builds/SetaiPressAvalon2.png" target="_blank">Avalon Press 2</a>
+        </span>
+    </div>
+  
+    
+    `});
