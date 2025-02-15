251dd4b87c108e233b96198f7ac21a9632aa9931
LuizFried <luifried50@gmail.com>
2025-02-15 18:42:34 -0300

    correção

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,8 @@
 document.getElementById("buildsID").addEventListener("click", function() {
     document.getElementById("conteudoID").innerHTML =`
+    <a href="#" onclick="location.reload(); return false;">
+        <i class="fa-solid fa-arrow-left"></i>
+    </a>
     <div class="conteudo__builds">
         <h1 class="titulo__builds">AVALON</h1>
         <span class="builds">
